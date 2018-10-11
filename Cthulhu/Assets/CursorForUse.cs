bca898a129f7f64fbeccb7185d60ee32debee329
Hao Bai <baihaozi12@gmail.com>
2018-10-11 23:15:18 +0100

    mouse click to move

---
+++ Cthulhu/Assets/CursorForUse.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorForUse : MonoBehaviour {
+    CameraRaycaster cameraRaycaster;
+	// Use this for initialization
+	void Start () {
+        cameraRaycaster = GetComponent<CameraRaycaster>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+       // print(cameraRaycaster.layerHit);
+	}
+}
