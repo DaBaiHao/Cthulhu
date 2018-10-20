f3c08a3f1411f0292be79158edddc881a5f102cb
Hao Bai <baihaozi12@gmail.com>
2018-10-20 23:38:01 +0100

    Add player health bar

---
+++ Cthulhu/Assets/Scenes/Player/Player.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float maxHealthPoints = 100f;
+    float currentHealthPoints = 100f;
+    public float healthAsPercentage
+    {
+        get
+        {
+            return currentHealthPoints / maxHealthPoints;
+        }
+    }
+}
